285a8cb028c548bd110e1d61c3e23cc78375c2d0
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-02-29 16:03:10 +0100

    first commit

---
+++ Ejercicio Práctico.py
@@ -0,0 +1,32 @@
+#Exercise 1
+string = "Rubens Ballester Lillo"
+number = 31
+boolean = True
+list = ["Rubens", "Ballester", "Lillo"]
+
+#Exercise 2
+string_mod = string[0:3]
+
+#Exercise 3
+list_first = list[0]
+
+#Exercise 4
+number_mod = number + 10
+
+#Exercise 5
+list_last = list[-1]
+
+#Exercise 6
+names = "harry,alex,susie,jared,gail,conner"
+names_list = names.split(",")
+
+#Exercise 7
+first_word = names_list[0].upper()
+new_string = first_word + names[5:]
+
+#Exercise 8
+string_inter = f"Hello my name is {string} and I am {number} years old"
+
+#Exercise 9
+print("Hello World")
+
